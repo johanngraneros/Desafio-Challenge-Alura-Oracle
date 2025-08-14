dd968f53328c28bb20426d7a8839c5115e4f5ec3
johanngraneros <granerosjohann05@gmail.com>
2025-08-14 19:39:50 -0300

    Agrego array para almacenar nombres, y CORRIJO la funcion "agregarAmigo"

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = []
+
 function validarEntrada(nombresSorteo) {
     if (nombresSorteo === "") {
         alert("Por favor, ingresa un nombre valido.");
@@ -13,7 +15,8 @@ function agregarAmigo() {
     
     if (!validarEntrada(nombresSorteo)) return; //Si el nombre no es valido, salgo de la funcion y no agrego nada a la lista.
     
-    let lista = document.getElementById("listaAmigos");
-    lista.innerHTML += nombresSorteo + "<br>";
+    nombresAmigos.push(nombresSorteo) //Agrego el nombre ingresado a la lista    
+    
     document.getElementById('amigo').value = ""; //limpia el campo de texto después de agregar un nombre a la lista
 }
+
