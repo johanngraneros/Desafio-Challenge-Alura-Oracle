7f58cd1eac58169652fa9f16bcf6fe9a90fa5c7b
johanngraneros <granerosjohann05@gmail.com>
2025-08-13 21:39:20 -0300

    Cambio el nombre de la variable "nombre" a "nombresSorteo" para que funcione bien.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-function validarEntrada(nombre) {
-    if (nombre === "") {
+function validarEntrada(nombresSorteo) {
+    if (nombresSorteo === "") {
         alert("Por favor, ingresa un nombre valido.");
         return false; // indica que no paso la validación
     }
@@ -11,9 +11,9 @@ function validarEntrada(nombre) {
 function agregarAmigo() {
     let nombresSorteo = document.getElementById('amigo').value
     
-    if (!validarEntrada(nombre)) return; //Si el nombre no es valido, salgo de la funcion y no agrego nada a la lista.
+    if (!validarEntrada(nombresSorteo)) return; //Si el nombre no es valido, salgo de la funcion y no agrego nada a la lista.
     
     let lista = document.getElementById("listaAmigos");
-    lista.innerHTML += nombre + "<br>";
+    lista.innerHTML += nombresSorteo + "<br>";
     document.getElementById('amigo').value = ""; //limpia el campo de texto después de agregar un nombre a la lista
 }
