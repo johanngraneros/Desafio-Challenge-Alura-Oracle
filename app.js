7e23529125687f1c70ed8196586419cccdbf734a
johanngraneros <granerosjohann05@gmail.com>
2025-08-13 21:26:17 -0300

    Creo funcion de validacion

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function validarEntrada(nombre) {
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre valido.");
+        return false; // indica que no paso la validación
+    }
+    return true; // paso la validación
+}
+
+
